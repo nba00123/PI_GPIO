779c8775d158260812078665027c93a1681614f6
nba00123 <nba001234@gmail.com>
2016-01-27 01:06:01 +0800

    dis

--- c/dis.c
+++ c/dis.c
@@ -39,8 +39,8 @@ void loop()
 	digitalWrite(outputPin, LOW); // 保持发出超声波信号接口低电平
 	int distance = pulseIn(inputPin, HIGH); // 读出脉冲时间
 	distance= distance/58; // 将脉冲时间转化为距离（单位：厘米）
-	if(distance>0)printf("dis=%d\n",distance); //输出距离值 
-	delay(500); 
+	printf("dis=%d\n",distance); //输出距离值 
+	delay(50); 
 	if (distance >=50)
 	{//如果距离大于50厘米小灯亮起
 		digitalWrite(ledpin,HIGH);
