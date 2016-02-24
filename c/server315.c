824ce0061133b3f7f8a693a27bfb0f9451f64a99
nba00123 <nba001234@gmail.com>
2016-02-25 00:38:11 +0800

    net

--- c/server315.c
+++ c/server315.c
@@ -36,7 +36,7 @@ void loop()
 	int t = pulseIn(inputPin, LOW, 2); // 读出脉冲时间
 	//if(t<1)return;
 	t = pulseIn(inputPin, HIGH, 10); // 读出脉冲时间
-	if(t<5)return;
+	//if(t<5)return;
 	t = pulseIn(inputPin, LOW, 2); // 读出脉冲时间
 	// printf("%d",t);
 	//if(t<1)return;
